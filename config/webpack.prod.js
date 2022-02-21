f7b9d73e15bd63bd07f5543564c06cf0822141eb
johanse7 <johanrodriguez_14@hotmail.com>
2022-02-20 22:34:23 -0500

    add wepack config typéscript

---
+++ config/webpack.prod.js
@@ -0,0 +1,16 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common');
+
+/** @type {import('webpack').Configuration} */
+const prodConfig = {
+  mode: 'production',
+  devtool: 'source-map',
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+    },
+  },
+  performance: { hints: false },
+};
+
+module.exports = merge(common, prodConfig);
